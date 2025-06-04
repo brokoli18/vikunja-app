ea3d1a482c09df99b698fc93f69c9741c4ae6679
Berns <berns@pop-os.l3g-gb.ibmmobiledemo.com>
2025-06-04 14:42:16 +0100

    App wont build

---
+++ android/app/src/main/kotlin/io/vikunja/flutteringvikunja/TaskWidget.kt
@@ -0,0 +1,17 @@
+package io.vikunja.flutteringvikunja
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.SharedPreferences
+import es.antonborri.home_widget.HomeWidgetProvider
+
+class TaskListWidget : HomeWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray,
+        widgetData: SharedPreferences
+    ) {
+        TODO("Not yet implemented")
+    }
+}
