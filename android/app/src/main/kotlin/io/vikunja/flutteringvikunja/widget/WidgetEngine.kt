74598592e0bfb09c90e12835b62260911dc525d8
Berns <berns@pop-os.tail62dfc.ts.net>
2025-08-14 14:58:27 +0100

    Remove unneeded imports

--- android/app/src/main/kotlin/io/vikunja/flutteringvikunja/widget/WidgetEngine.kt
+++ android/app/src/main/kotlin/io/vikunja/flutteringvikunja/widget/WidgetEngine.kt
@@ -1,20 +1,14 @@
 package io.vikunja.flutteringvikunja.widget
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
-import androidx.work.ListenableWorker.Result
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import io.flutter.FlutterInjector
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.dart.DartExecutor
 import io.flutter.plugin.common.MethodChannel
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeoutException
 
 class FlutterWorker(private val context: Context, params: WorkerParameters) :
     Worker(context, params) {
